ef9c9ec567727c406e3b7cf1a8e51328d009271f
Joaquin Castillo <joaquinacv04@gmail.com>
2025-10-17 02:11:53 -0300

    feat: Conectar frontend a la URL de producción de la API

--- frontend/angular-mongodb-app/src/environments/environment.prod.ts
+++ frontend/angular-mongodb-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // URL de backend en Vercel
-  apiUrl: 'https://proyecto-mongodb-angular-q6yp.vercel.app/api'
+  apiUrl: 'https://proyecto-mongodb-angular-6gju.vercel.app/api'
 };
